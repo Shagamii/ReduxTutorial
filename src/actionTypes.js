41632594f026f54924a532324575437b699ba2dc
Ryuichi Sakagami <shagamiiiii@gmail.com>
2016-12-31 17:09:57 +0900

    add actionType

---
+++ src/actionTypes.js
@@ -0,0 +1,10 @@
+export const actionTypes = {
+  TODO: {
+    ADD_TODO: 'TODO_ADD_TODO',
+    TOGGLE_TODO: 'TODO_TOGGLE_TODO'
+  },
+  TODOS: {
+    ADD_TODO: 'TODOS_ADD_TODO',
+    TOGGLE_TODO: 'TODOS_TOGGLE_TODO'
+  }
+};
